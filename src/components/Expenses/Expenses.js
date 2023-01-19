65b289140f2c756bd8a036ce31a259231d1725d9
Carlos Ortiz <calitos.eos@gmail.com>
2023-01-19 13:28:55 -0500

    feat: add Chart component

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -3,6 +3,7 @@ import ExpensesFilter from './ExpensesFilter'
 import ExpensesList from './ExpensesList'
 import Card from '../UI/Card'
 import './Expenses.css'
+import ExpensesChart from './ExpensesChart'
 
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState('2019')
@@ -22,7 +23,8 @@ const Expenses = (props) => {
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
-        <ExpensesList items={filteredExpenses}/>
+        <ExpensesChart expenses={filteredExpenses} />
+        <ExpensesList items={filteredExpenses} />
       </Card>
     </div>
   )
